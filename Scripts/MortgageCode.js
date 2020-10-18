7011034e7b43cc1c8bdefdf9897474e6c8b13553
Jon <71294685+october464@users.noreply.github.com>
2020-10-18 14:16:29 -0400

    update to table
    created the array and it prints out the table but still a little buggy

--- Scripts/MortgageCode.js
+++ Scripts/MortgageCode.js
@@ -35,24 +35,7 @@
     document.getElementById("remainBalance").innerHTML = "$" + rBalance;
     document.getElementById("princialPayment").innerHTML = "$" + prinPayment;
 
-    //let table = document.getElementById("results");
-    ////inserts row at the end of the table 
-    //let row = table.insertRow(9);
-    ////inserts the cell in the row
-    //let cell1 = row.insertCell(0);
-    //let cell2 = row.insertCell(1);
-    //let cell3 = row.insertCell(2);
-    //let cell4 = row.insertCell(3);
-    //let cell5 = row.insertCell(4);
-    //let cell6 = row.insertCell(5);
     
-    ////outputs in the row
-    ////cell1.innerHTML = ;
-    //cell2.innerHTML = monthlyPayment;
-    //cell3.innerHTML = prinPayment;
-    //cell4.innerHTML = totalInterest
-    //cell5.innerHTML = totalPayment;
-    //cell6.innerHTML = rBalance;
     var intRate = 0;
     let balance = principal;
     //create new array 
@@ -60,29 +43,30 @@
     for (i = 0; i < months && balance > 0; i++) {
         intRate += parseFloat(balance * (monthly));
         array[i] = new Array();
-        array[i][0] = (i + 1).toFixed(0);
-        array[i][1] = parseFloat(x).toFixed(2);
-        array[i][2] = parseFloat(x - (balance * (monthly))).toFixed(2);
+        array[i][0] = (i + 1).toFixed(2);
+        array[i][1] = parseFloat(monthlyPayment).toFixed(2);
+        array[i][2] = parseFloat(monthlyPayment - (balance * (monthly))).toFixed(2);
         array[i][3] = parseFloat(balance * (monthly)).toFixed(2);
         array[i][4] = parseFloat(intRate).toFixed(2);
-        array[i][5] = parseFloat(balance - x).toFixed(2);
-        if (x > balance) {
-            arr[i][1] = arr[i - 1][5];
-            arr[i][2] = arr[i][1] - arr[i][3];
-            arr[i][5] = 0.00;
+        array[i][5] = parseFloat(balance - monthlyPayment).toFixed(2);
+        if (monthlyPayment > balance) {
+            array[i][1] = array[i - 1][5];
+            array[i][2] = array[i][1] - array[i][3];
+            array[i][5] = 0.00;
         }
-        balance -= parseFloat(x).toFixed(2);
+        balance -= parseFloat(monthlyPayment).toFixed(2);
 
 
     }
-
+    //takes the id and creates a new row in the table
     var table = document.getElementById("results").getElementsByTagName("tbody")[0];
     for (let j = 0; table.rows.length > 0; j++) {
-        table.deleteRow(-1);
+        table.deleteRow(-1);//deletes the last roww in the table
     }
-    for (let k = 0; k < months; k++) {
+    //
+    for (let i = 0; i < months; i++) {
         let newRow = table.insertRow();
-        var myTable = `<td>${(array[k][0])}</td><td>${(array[k][1])}</td><td>${(array[k][2])}</td><td>${(array[k][3])}</td><td>${(array[k][4])}</td><td>${(array[k][5])}</td>`;
+        var myTable = `<td>${(array[i][0])}</td><td>${(array[i][1])}</td><td>${(array[i][2])}</td><td>${(array[i][3])}</td><td>${(array[i][4])}</td><td>${(array[i][5])}</td>`;
         newRow.innerHTML = myTable;
     }
     return;
